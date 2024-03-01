2d2112495690c0d59d0adb872cca0feb6b1348c4
PrateekTechmatys <prateeky562@gmail.com>
2024-03-01 13:51:34 +0530

    completed

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 let main = document.querySelector('.main');
 let circle = document.getElementById("circle");
-
+let text = document.getElementById("text");
 
 
 console.log(main,circle)
-
+let online = true;
+circle.addEventListener('click',function(){
+    if(online){
+        circle.classList.add('dark-mode');
+    main.style.backgroundColor = "black";
+    text.style.color="white";
+    online=false;
+    }
+    else{
+        circle.classList.remove('dark-mode');
+        main.style.backgroundColor = "white";
+        text.style.color="black";
+        online=true;
+    }
+})
